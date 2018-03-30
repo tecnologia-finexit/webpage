42ae71399cc57d5c65c14d4bb8a5f49c60b27726
DavidGibran <david.gibran@gmail.com>
2018-03-30 12:25:57 -0600

    Add - Menu and header section

--- Finexit/src/com/fin/web/commons/ApplicationBean.java
+++ Finexit/src/com/fin/web/commons/ApplicationBean.java
@@ -12,11 +12,16 @@ public class ApplicationBean implements Serializable{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 6006916580801632409L;
-	private final String COMPANY_NAME ="finexit";
+	private static final long serialVersionUID = -8858450532723384349L;
+	private final String COMPANY_NAME ="Finexit";
+	private final String URL_HOME = "http://35.224.55.235/";
 
 	public String getCOMPANY_NAME() {
 		return COMPANY_NAME;
+	}
+
+	public String getURL_HOME() {
+		return URL_HOME;
 	}
 	
 }
--- Finexit/src/com/fin/web/ui/IndexBean.java
+++ Finexit/src/com/fin/web/ui/IndexBean.java
@@ -1,6 +1,8 @@
 package com.fin.web.ui;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.enterprise.context.ConversationScoped;
 import javax.inject.Named;
@@ -12,11 +14,19 @@ public class IndexBean implements Serializable{
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -6990116898145987614L;
+	private static final long serialVersionUID = -3221821742903072374L;
 	private String message;
+	private List<String> list;
 
 	public IndexBean() {
 		super();
+		
+		this.list = new ArrayList<>();
+		int j = new Double( Math.random()*30).intValue();
+		for(int i =0; i<j;i++) {
+			list.add("Item " + i); 
+		}
+		
 	}
 
 	public String getMessage() {
@@ -27,9 +37,21 @@ public class IndexBean implements Serializable{
 		this.message = message;
 	}
 
+	public List<String> getList() {
+		return list;
+	}
+
+	public void setList(List<String> list) {
+		this.list = list;
+	}
+
 	@Override
 	public String toString() {
 		return "IndexBean [message=" + message + "]";
+	}
+	
+	public void testMethod() {
+		System.out.println("Hi, I'm a method");
 	}
 	
 }
